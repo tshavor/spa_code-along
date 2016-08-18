7f4c3756f717303187338975b381143558bbc997
Thomas J. Shavor <thomasshavor@gmail.com>
2016-08-18 11:10:14 -0500

    saving grunt

---
+++ js/quiz.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var carInventory= require('./carLotPromises'),
+event stuff= require(',/events');
+
+
+// (function(){
+  function populatePage (inventory) {
+    var output = document.querySelector(".output")
+    var results = ""
+    inventory.forEach(function(car, i){
+      if (i % 3 === 0) {
+      results += `<div class="row">`
+    }
+      results += `
+      <div class="col-md-4 carCard" style="border-color: ${car.color}">
+      <h3>${car.make}</h3>
+      <h3>${car.model}</h3>
+      <h3>${car.year}</h3>
+      <h3>$${car.price}</h3>
+      <h3>${car.color}</h3>
+      <h3>${car.purchased}</h3>
+      <p>${car.description}</p>
+      </div>
+      `
+      if ((i + 1) % 3 === 0) {
+    results += `</div>`
+    }
+    })
+    output.innerHTML = results
+
+    CarLot.activateEvents();
+  }
+
+  CarLot.loadInventory(populatePage);
+})();
